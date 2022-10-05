7b3e4d65eff9b4bccf15a94953faef1241bb9f6a
Fabian NP <fabian.nava.pereda@gmail.com>
2022-10-05 01:03:53 -0500

    Finally finish the know me page, maked of a lot animations with differents dependencies like 'gsap, animate.css' for the animations 'react-leaflet for the map, 'emailjs' to submit the form contact, using react router to display the differents pages and finally making a cube box animation from scratch using 6 differents <div's to make each face of the cube the rotate the div parent to show all of them, and at least using sass to write code efficiently and use the function for inside to display letters with differents index in a class and set the delay one before other making a beatiful animation of fadein letter by letter

---
+++ my-app/src/components/Contact/Contact.js
@@ -0,0 +1,100 @@
+import { useEffect, useRef, useState } from 'react'
+import Loader from 'react-loaders'
+import AnimatedLetters from '../AnimatedLetters/Animated'
+import './contact.scss'
+import 'animate.css'
+import emailjs from '@emailjs/browser'
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
+
+
+const Contact = () => {
+  const [letterClass, setLetterClass] = useState('text-animate')
+  const strContact = 'Contact me'
+  const arrContact = [...strContact] 
+  const refForm = useRef()
+
+  useEffect(() => {
+    setTimeout (() => {
+      setLetterClass('text-animate-hover')
+    }, 4000)
+  }, []);
+
+  const sendEmail = (e) => {
+    e.preventDefault()
+
+    emailjs
+        .sendForm(
+          'service_k0dp23r',
+          'template_j1oytji',
+          refForm.current,
+          'jb-eyXnVKVhPnSrn5'
+        )
+        .then(
+          () => {
+            alert('Message successfully sent!')
+            window.location.reload(false)
+          },
+          (err) => {
+            console.log(err)
+          }
+        )
+  }
+
+  return(
+    <>
+      <div className='container contact-page'>
+        <div className='text-zone'>
+          <h1>
+            <AnimatedLetters strArray={arrContact} letterClass={letterClass} idx={15}/>
+          </h1>
+          <p>
+            Ea est occaecat quis pariatur sint cupidatat sunt anim sunt sit. Ea consectetur aute laboris exercitation aliqua exercitation aute eiusmod in proident officia. Eu dolor nulla id irure irure aliqua eiusmod excepteur adipisicing dolore reprehenderit laboris tempor velit. Eiusmod duis irure dolor laborum non minim ullamco. Veniam anim esse eu enim.
+          </p>
+          <div className='contact-form'> 
+            <form ref={refForm} onSubmit={sendEmail}>
+              <ul>
+                <li className='half'>
+                  <input type="text" name='name' placeholder='Name' required/>
+                </li>
+                <li className='half'>
+                  <input type="email" name='email' placeholder='Email' required/>
+                </li>
+                <li>
+                  <input placeholder='Subject' type='text' name='subject' required />
+                </li>
+                <li>
+                  <textarea placeholder='Message' name='message' required></textarea>
+                </li>
+                <li>
+                  <input type="submit" className='flat-button' value='SEND' />
+                </li>
+              </ul>
+            </form>
+          </div>
+        </div>
+        <div className='info-map'>
+          Fabian,
+          <br/>
+          Nava Pereda,
+          <br/>
+          Real Alcala,
+          <br/>
+          Ojo de agua
+          <br/>
+          <span>fabian.nava.pereda@gmail.com</span>
+        </div>
+        <div className='map-wrap'>
+          <MapContainer center={[19.643999, -98.978551]} zoom={23}>
+            <TileLayer url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png' />
+            <Marker position={[19.643999, -98.978551]}> 
+              <Popup>Fabian lives here, come here in a sunny day or not, just bring ice cream and lets talk</Popup>
+            </Marker>
+          </MapContainer >
+        </div>
+      </div>
+      <Loader type='pacman'/>
+    </>
+  )
+}
+
+export default Contact
---
+++ my-app/src/components/Sidebar/Sidebar.js
@@ -0,0 +1,48 @@
+import './sidebar.scss'
+import LogoS from '../../assets/images/logo-s.png'
+import { Link, NavLink } from 'react-router-dom'
+import LogoSubtitle from '../../assets/images/logo_sub.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons'
+import { faGithub, faLinkedin, faYoutube } from '@fortawesome/free-brands-svg-icons'
+
+const Sidebar = () => {
+  return (
+    <div className='nav-bar'>
+      <Link className='logo' to='/'>
+        <img src={LogoS} alt="logo" />
+        <img src={LogoSubtitle} alt="slobodan" className='sub-logo'/>
+      </Link>
+      <nav>
+        <NavLink exact='true'  to='/' end>
+          <FontAwesomeIcon icon={faHome} color='#4d4d4e'/>
+        </NavLink>
+        <NavLink exact='true'  className='about-link' to='/about'>
+          <FontAwesomeIcon icon={faUser} color='#4d4d4e'/>
+        </NavLink>
+        <NavLink exact='true'  className='contact-link' to='/contact'>
+          <FontAwesomeIcon icon={faEnvelope} color='#4d4d4e'/>
+        </NavLink>
+      </nav>
+      <ul>
+        <li>
+          <a target='_blank' rel='noreferrer' href="https://www.linkedin.com/in/fabian-n-1ab288173/">
+            <FontAwesomeIcon icon={faLinkedin} color='#4d4d4e'/>
+          </a>
+        </li>
+        <li>
+          <a target='_blank' rel='noreferrer' href="https://github.com/Empyritz">
+            <FontAwesomeIcon icon={faGithub} color='#4d4d4e'/>
+          </a>
+        </li>
+        <li>
+          <a target='_blank' rel='noreferrer' href="https://music.youtube.com/watch?v=OkEFHhDBK74&list=RDAMVMOkEFHhDBK74">
+            <FontAwesomeIcon icon={faYoutube} color='#4d4d4e'/>
+          </a>
+        </li>
+      </ul>
+    </div>
+  )
+}
+
+export default Sidebar
